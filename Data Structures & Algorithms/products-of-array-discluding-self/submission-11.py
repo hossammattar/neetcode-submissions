46874de2da264f3937408729027573c8e067ef12
hossammattar <85787446+hossammattar@users.noreply.github.com>
2026-07-22 07:31:42 +0300

    Add: products-of-array-discluding-self - submission-11

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-11.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product = 1
+        zero_count = 0
+        for num in nums:
+            if num:
+                product *= num
+            else:
+                zero_count += 1
+        res = []
+        for num in nums:
+            if zero_count == 0:
+                res.append(product // num)
+            elif not num and (zero_count == 1):
+                res.append(product)
+            elif zero_count > 0:
+                res.append(0)
+
+        return res
